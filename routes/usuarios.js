a4816a25e55887482a73825dabcf2cc042e94ade
Ricardo Pérez <rpe19971210@gmail.com>
2022-01-06 00:27:41 -0300

    Middleware personalizado

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const { getUsuarios, crearUsuario } = require('../controllers/usuarios');
 
@@ -11,7 +12,8 @@ router.post( '/',
     [
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
         check('password', 'El password es obligatorio').not().isEmpty(),
-        check('email', 'El email es obligatorio').isEmail()
+        check('email', 'El email es obligatorio').isEmail(),
+        validarCampos
     ],
     crearUsuario 
 );
