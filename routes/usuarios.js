cf9c0ca5f7ae982c847be6ccfdead9c4708b514e
Ricardo Pérez <rpe19971210@gmail.com>
2022-01-07 01:31:39 -0300

    Middleware de JWT

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,10 +3,11 @@ const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
-router.get( '/', getUsuarios );
+router.get( '/', validarJWT, getUsuarios );
 
 router.post( '/',
     [
@@ -20,15 +21,19 @@ router.post( '/',
 
 router.put( '/:id',
     [
+        validarJWT,
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
-        check('role', 'El role es obligatorio').isEmail(),
+        check('role', 'El role es obligatorio').not().isEmpty(),
         validarCampos
     ],
     actualizarUsuario 
 );
 
-router.delete('/:id', borrarUsuario);
+router.delete('/:id',
+    validarJWT, 
+    borrarUsuario
+);
 
 
 
