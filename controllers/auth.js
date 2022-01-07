0f104dff244f0e734a560c976690ef78aaf57fe7
Ricardo Pérez <rpe19971210@gmail.com>
2022-01-07 00:41:54 -0300

    Uso de JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const bcrypt = require("bcryptjs");
 
 const Usuario = require('../models/usuario');
+const { generarJWT } = require("../helpers/jwt");
 
 const login = async( req, res = response ) => {
 
@@ -29,10 +30,11 @@ const login = async( req, res = response ) => {
         }
 
         // Generar el TOKEN - JWT
+        const token = await generarJWT( usuarioDB.id );
 
         res.json({
             ok: true,
-            msg: 'Hola Mundo'
+            token
         });
         
     } catch (error) {
--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
+const { generarJWT } = require('../helpers/jwt');
 
 
 const getUsuarios = async(req, res) => {
@@ -38,10 +39,14 @@ const crearUsuario = async(req, res = response) => {
 
         // Guardar usuario
         await usuario.save();
+
+        // Generar el TOKEN - JWT
+        const token = await generarJWT( usuario.id );
     
         res.json({
             ok: true,
-            usuario
+            usuario,
+            token
         });
         
     } catch (error) {
