c120aef8f4d3be62b52f5704edb7b1e8c608c4b5
Ricardo Pérez <rpe19971210@gmail.com>
2022-01-08 02:24:22 -0300

    CRUD de Medicos

--- routes/medicos.js
+++ routes/medicos.js
@@ -1,26 +1,26 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-
 const { validarCampos } = require('../middlewares/validar-campos');
 
+const { getMedicos, crearMedico, actualizarMedico, borrarMedico } = require('../controllers/medicos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
-router.get( '/', getHospitales );
+router.get( '/', getMedicos );
 
 router.post( '/',
     [],
-    crearHospital 
+    crearMedico 
 );
 
 router.put( '/:id',
     [],
-    actualizarHospital 
+    actualizarMedico 
 );
 
 router.delete('/:id',
-    borrarHospital
+    borrarMedico
 );
 
 
