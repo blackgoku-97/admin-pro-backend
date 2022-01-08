fd58ec9e90ff2ff655bfaf9daca31610f096ea70
Ricardo Pérez <rpe19971210@gmail.com>
2022-01-07 23:05:23 -0300

    CRUD de Hospitales

---
+++ routes/hospitales.js
@@ -0,0 +1,29 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { getHospitales, crearHospital, actualizarHospital, borrarHospital } = require('../controllers/hospitales');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+router.get( '/', getHospitales );
+
+router.post( '/',
+    [],
+    crearHospital 
+);
+
+router.put( '/:id',
+    [],
+    actualizarHospital 
+);
+
+router.delete('/:id',
+    borrarHospital
+);
+
+
+
+
+module.exports = router;
